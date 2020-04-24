c7889ece38984586d15b0b8e1f33e01131916a26
goodswifter <1042480866@qq.com>
2020-04-24 22:58:22 +0800

    1

--- study_flutter/lib/main.dart
+++ study_flutter/lib/main.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  final imageURL =
+      "https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1587721847&di=b7b3b1f2882f7e015ebc321dff2e672e&src=http://pic1.win4000.com/wallpaper/2017-10-30/59f68513a33ca.jpg";
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,22 +18,11 @@ class MyApp extends StatelessWidget {
           onPressed: () => print("FloatingActionButton Click"),
         ),
         floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-        body: ADHomeContent(),
+
+        body: Container(),
       ),
     );
   }
 }
-
-class ADHomeContent extends StatelessWidget {
-  final imageURL = "https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1587721847&di=b7b3b1f2882f7e015ebc321dff2e672e&src=http://pic1.win4000.com/wallpaper/2017-10-30/59f68513a33ca.jpg";
-
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
-
-
-
 
 
