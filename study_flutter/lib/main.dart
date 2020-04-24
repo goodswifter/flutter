ecb275c60a09826eddb6fc4fbd02261f6b8546db
goodswifter <1042480866@qq.com>
2020-04-24 18:02:40 +0800

    【修改内容】 :  1 【reviewer】  :  钟爱东

--- study_flutter/lib/main.dart
+++ study_flutter/lib/main.dart
@@ -7,85 +7,30 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("StatefulWidget的生命周期"),),
-        body: ADHomePage(),
+        appBar: AppBar(
+          title: Text("基础Widget"),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () => print("FloatingActionButton Click"),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        body: ADHomeContent(),
       ),
     );
   }
 }
 
-class ADHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return ADStatefulWidget();
-  }
-}
-
-class ADStatefulWidget extends StatefulWidget {
-
-  ADStatefulWidget() {
-    print("1. 调用ADStatefulWidget的constructor方法");
-  }
-
-  @override
-  _ADStatefulWidgetState createState() {
-    print("2. ADStatefulWidget - createState");
-    return _ADStatefulWidgetState();
-  }
-}
-
-class _ADStatefulWidgetState extends State<ADStatefulWidget> {
-  int _counter = 0;
-
-  _ADStatefulWidgetState() {
-    print("3. 调用_ADStatefulWidgetState的constructor方法");
-  }
-
-  @override
-  // 注意: 如果父类有这个关键@mustCallSuper, 子类重写父类方法时, 必须调用父类的这个方法
-  void initState() {
-    super.initState();
-    print("4. 调用_ADStatefulWidgetState的initState方法");
-  }
-
-  /// 什么时候调用?
-  @override
-  void didUpdateWidget(ADStatefulWidget oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    print("state - didUpdateWidget");
-  }
-
-  /// 什么时候调用?
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    print("state - didChangeDependencies");
-  }
+class ADHomeContent extends StatelessWidget {
+  final imageURL = "https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1587721847&di=b7b3b1f2882f7e015ebc321dff2e672e&src=http://pic1.win4000.com/wallpaper/2017-10-30/59f68513a33ca.jpg";
 
   @override
   Widget build(BuildContext context) {
-    print("5. 调用_ADStatefulWidgetState的build方法");
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          RaisedButton(
-            child: Icon(Icons.add),
-            onPressed: () {
-              setState(() {
-                _counter++;
-              });
-            },
-          ),
-          Text("数字:$_counter")
-        ],
-      ),
-    );
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    print("6. 调用_ADStatefulWidgetState的dispose方法");
+    return Container();
   }
 }
+
+
+
+
+
