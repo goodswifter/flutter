58dd568ac1a6ba40e086eb663b3d7fe1288edc50
goodswifter <1042480866@qq.com>
2020-04-26 17:50:32 +0800

    【修改内容】 :  1 【reviewer】  :  钟爱东

--- study_flutter/lib/main.dart
+++ study_flutter/lib/main.dart
@@ -3,9 +3,6 @@ import 'package:flutter/material.dart';
 main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  final imageURL =
-      "https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1587721847&di=b7b3b1f2882f7e015ebc321dff2e672e&src=http://pic1.win4000.com/wallpaper/2017-10-30/59f68513a33ca.jpg";
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,10 +16,15 @@ class MyApp extends StatelessWidget {
         ),
         floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
 
-        body: Container(),
+        body: ADHomeContent(),
       ),
     );
   }
 }
 
-
+class ADHomeContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
