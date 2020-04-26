351bbc21e0f4e78f33787e7b6a1c7e497092563d
goodswifter <1042480866@qq.com>
2020-04-26 21:13:37 +0800

    1

--- study_flutter/lib/main.dart
+++ study_flutter/lib/main.dart
@@ -15,7 +15,6 @@ class MyApp extends StatelessWidget {
           onPressed: () => print("FloatingActionButton Click"),
         ),
         floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-
         body: ADHomeContent(),
       ),
     );
