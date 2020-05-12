4fb5cfa9e523ea6c55780fc51ba1bad425dabda8
goodswifter <1042480866@qq.com>
2020-05-12 23:52:46 +0800

    1

---
+++ study_flutter/lib/day10/main.dart
@@ -0,0 +1,31 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Flutter Demo"),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () => print("FloatingActionButton Click"),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        body: ADHomeContent(),
+      ),
+    );
+  }
+}
+
+class ADHomeContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
